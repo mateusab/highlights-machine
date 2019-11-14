b27aa22b03af0d0b12540b4e433f063b09819658
xips <xips@gamersclub.com.br>
2019-11-14 15:26:35 -0200

    print kills per round

--- main.go
+++ main.go
@@ -5,15 +5,23 @@ import (
 	"os"
 
 	dem "github.com/markus-wa/demoinfocs-golang"
-	// events "github.com/markus-wa/demoinfocs-golang/events"
+	events "github.com/markus-wa/demoinfocs-golang/events"
 )
+
+type player struct {
+	name  string
+	kills int
+}
 
 func main() {
 	// pega os argumentos passados como parametro
 	args := os.Args[1:]
 
+	round := 0
+	roundStart := 0
+	roundEnd := 0
 	// abre a demo
-	f, err := os.Open("./demos/mibr-vs-eunited.dem")
+	f, err := os.Open("./demos/mibr.dem")
 	if err != nil {
 		panic(err)
 	}
@@ -24,9 +32,35 @@ func main() {
 	header, err := p.ParseHeader()
 	fmt.Println("Nome da demo: ", args, "\nMapa: ", header.MapName)
 
+	p.RegisterEventHandler(func(e events.RoundStart) {
+		round++
+		roundStart = round
+	})
+
+	p.RegisterEventHandler(func(e events.RoundEndOfficial) {
+		roundEnd = round
+		fmt.Println("Round", roundEnd)
+	})
+
+	if roundStart == roundEnd {
+		p.RegisterEventHandler(func(e events.Kill) {
+			killCounter(e)
+		})
+	}
+
 	// parse to end
 	err = p.ParseToEnd()
 	if err != nil {
 		panic(err)
+	}
+}
+
+func killCounter(e events.Kill) {
+	fmt.Println(e.Killer.Name, "killed", e.Victim.Name)
+}
+
+func diedBlind(e events.Kill, tickNumber int) {
+	if e.Victim.IsBlinded() {
+		fmt.Println("[tick: ", tickNumber, "]", e.Victim.Name, "died blinded")
 	}
 }
